0455cbded26d52b59bcca376fd81a92cf8f9cf67
3H2T <lengochieu732001@gmail.com>
2021-11-13 10:44:26 +0700

    sửa

--- app/src/main/java/com/example/gas247/Activity/TrangchuActivity.java
+++ app/src/main/java/com/example/gas247/Activity/TrangchuActivity.java
@@ -1,14 +1,16 @@
-package com.example.gas247;
+package com.example.gas247.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gas247.R;
+
+public class TrangchuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_trangchu);
     }
 }
